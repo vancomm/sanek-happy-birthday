90bd7981e384e49d368571861e6e330f52a8ded5
vancomm <peshekhonov.ip@gmail.com>
2023-07-07 03:15:07 +0300

    Added wow

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+const ZERO_WIDTH_SPACE = "​";
+
+
+/** @type {HTMLSpanElement} */
+const congratulationSpan = document.querySelector('#congratulation');
+
+congratulationSpan.addEventListener("click", () => {
+    congratulationSpan.textContent += `1${ZERO_WIDTH_SPACE}`;
+});
+
+
 /** @type {HTMLTemplateElement} */
 const balloonsTemplate = document.querySelector("template#balloons");
 
 [...Array(10).keys()]
-    .map((_, i) => {
-        const left = `${i * 10}%`
-        const delay = `${Math.random() * 2.5}s`
-        const zIndex = Math.random() > .75 ? "100" : "0"
+    .forEach((_, i) => {
         /** @type {HTMLImageElement} */
-        const balloonsElem = balloonsTemplate.content.cloneNode(true).querySelector('img')
-        balloonsElem.style.cssText += `--left: ${left}; --delay: ${delay}; --z-index: ${zIndex}`
-        return balloonsElem
-    })
-    .forEach((el) => {
-        document.body.appendChild(el)
-    })
+        const balloonsElem = balloonsTemplate.content.cloneNode(true).querySelector('img');
+        const left = `${i * 10}%`;
+        const delay = `${Math.random() * 2.5}s`;
+        const zIndex = Math.random() > .75 ? "100" : "0";
+        balloonsElem.style.cssText += `--left: ${left}; --delay: ${delay}; --z-index: ${zIndex}`;
+        document.body.appendChild(balloonsElem);
+    });
